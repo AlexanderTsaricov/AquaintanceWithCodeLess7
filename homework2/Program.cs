c581537c9de691504ac0d9ed8bd5c25268610fcf
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-04-05 16:04:46 +0200

    add less7:homework2

---
+++ homework2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+    Даны два неотрицательных числа m и n.
+    Ответ: Исходя из формулы, что я нашел
+    Формула:
+    А(0, т) = n+1
+    A(m, 0) = A(m-1, 1)
+    A(m, n) = A(m-1, A(m, n-1)),
+    где насколько я понял, если не 0, значит
+    больше 0
+*/
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0)
+    {
+        return AkkermanFunction(m - 1, 1);
+    }
+    return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+Console.WriteLine(AkkermanFunction(1, 2));
+
