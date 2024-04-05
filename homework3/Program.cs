0bf9b0dbac08709a11df6ee0fd6fdafafcf9b869
Alexandr Vinokurov <salispiligrim@yandex.ru>
2024-04-05 16:30:18 +0200

    add less7:homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+    Задайте произвольный массив. 
+    Выведете его элементы, начиная с конца. 
+    Использовать рекурсию, не использовать циклы.
+*/
+int i = 0;
+Random rnd = new Random();
+int[] FillingArray(int[] arr, int i)
+{
+    if (i >= arr.Length)
+    {
+        return arr;
+    }
+    arr[i] = rnd.Next(1, 10);
+    return arr = FillingArray(arr, ++i);
+}
+
+int[] array = new int[10];
+array = FillingArray(array, i);
+
+int endNum = array.Length - 1;
+int firstNum = 0;
+
+void FlipPrintArray(int[] arr, int endNum)
+{
+    if (endNum < 0)
+    {
+        return;
+    }
+    Console.Write($"{arr[endNum]} ");
+    FlipPrintArray(arr, --endNum);
+}
+
+void PrintArray(int[] arr, int firstNum)
+{
+    if (firstNum >= arr.Length)
+    {
+        return;
+    }
+    Console.Write($"{arr[firstNum]} ");
+    PrintArray(arr, ++firstNum);
+}
+
+Console.WriteLine("Рандомный массив:");
+PrintArray(array, firstNum);
+Console.WriteLine();
+Console.WriteLine("Перевернутый массив:");
+FlipPrintArray(array, endNum);
